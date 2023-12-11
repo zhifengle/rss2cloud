8571fe9959ae8bc363a59ee2849367a8b450adf4
zhifengle <zhifengle@gmail.com>
2023-12-11 09:25:42 +0800

    update: text links

--- rsssite/utils.go
+++ rsssite/utils.go
@@ -6,6 +6,15 @@ import (
 	"strings"
 )
 
+func HasPrefix(str string, prefixArr []string) bool {
+	for _, prefix := range prefixArr {
+		if strings.HasPrefix(str, prefix) {
+			return true
+		}
+	}
+	return false
+}
+
 func GetMagnetsFromText(textFile string) ([]string, error) {
 	file, err := os.Open(textFile)
 	if err != nil {
@@ -14,10 +23,11 @@ func GetMagnetsFromText(textFile string) ([]string, error) {
 	defer file.Close()
 
 	var lines []string
+	prefixArr := []string{"magnet:", "ed2k://", "https://", "http://", "ftp://"}
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		text := scanner.Text()
-		if strings.HasPrefix(text, "magnet:?xt=urn:btih:") || strings.HasPrefix(text, "ed2k://|file|") {
+		if HasPrefix(text, prefixArr) {
 			lines = append(lines, text)
 		}
 	}
