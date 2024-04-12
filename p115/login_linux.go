3d96faf7b95c8886c5afde48cc7f2ea35034168e
zhifengle <zhifengle@gmail.com>
2024-04-12 14:56:13 +0800

    update: linux open qrcode

--- p115/login_linux.go
+++ p115/login_linux.go
@@ -1,10 +1,41 @@
 package p115
 
-import "errors"
+import (
+	"errors"
+	"os"
+	"os/exec"
+)
+
+func getValidOpenCommand() string {
+	commnads := []string{"xdg-open", "gnome-open", "kde-open"}
+	for _, c := range commnads {
+		_, err := exec.LookPath(c)
+		if err == nil {
+			return c
+		}
+	}
+	return ""
+}
 
 func DisplayQrcode(img []byte) error {
-	return errors.New("Qrcode login is not support on Linux")
+	imageName := "qrcode115.png"
+	// save image
+	err := os.WriteFile(imageName, img, 0644)
+	if err != nil {
+		return err
+	}
+	openCommand := getValidOpenCommand()
+	if openCommand == "" {
+		return errors.New("no open command found")
+	}
+	cmd := exec.Command(openCommand, imageName)
+	err = cmd.Start()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func DisposeQrcode() {
+	_ = os.Remove("qrcode115.png")
 }
